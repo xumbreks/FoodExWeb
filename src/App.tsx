f3ae5a9631c8bc366e1b52bd2f9021685da96819
Brian Rangel <brian.rangel@hotmail.com>
2024-01-31 20:34:06 +0000

    Initial config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./global.css";
+import { Button } from "@/components/ui/button";
+
+export function App() {
+  return (
+    <div>
+      <Button className="">Click me</Button>
+    </div>
+  );
+}
